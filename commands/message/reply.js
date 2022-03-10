bd60f9b284a599dfcbe80bba1c9d03a4371867a3
Stark <58781687+Sitarkjs@users.noreply.github.com>
2022-03-10 19:30:14 +0300

    Update reply.js

--- commands/message/reply.js
+++ commands/message/reply.js
@@ -3,7 +3,7 @@ module.exports = {
 		name: "repeat",
 		type: 3
 	},
-	run: async (client, interaction) => {
+	run: async (client, m, interaction) => {
 		const channel = interaction.guild.channels.cache.get(interaction.channelId);
 		const message = await channel.messages.fetch(interaction.targetId);
 		await interaction.reply({ content: message.content, ephemeral: true });
